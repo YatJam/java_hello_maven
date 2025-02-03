4c9e392c2e038808ce6fac6a8ded77db00a6f2be
James Yates <114536139+YatJam@users.noreply.github.com>
2025-02-03 15:33:09 +0000

    Add unit tests for health class.

---
+++ src/test/java/HealthTest.java
@@ -0,0 +1,27 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.example.hellomaven.model.Health;
+
+public class HealthTest {
+
+  private Health health;
+
+  @Before
+  public void before(){
+    health = new Health("I am alive");
+  }
+
+  @Test
+  public void canGetMessage(){
+    assertEquals("I am alive", health.getMessage());
+  }
+
+  @Test
+  public void canChangeMessage(){
+    health.setHealthMessage("I am still here");
+    assertEquals("I am still here", health.getMessage());
+  }
+}
